9e811213598529339ba8fb3701418fcd659fe844
Heberth Severo <hvinicius150@gmail.com>
2024-10-11 16:19:58 -0300

    Modulo 5 - Comunicacao com backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Pensamento} from "../pensamento";
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações para o componente filho',
-      autoria: 'Componente pai',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Minha propriedade é decorada com @Input()',
-      autoria: 'Componete filho',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Bombardeios israelenses em duas áreas diferentes no centro de Beirute, capital do Líbano, nesta quinta-feira (10) mataram 18 pessoas e deixaram ao menos 92 feridos, segundo o Ministério da Saúde libanês. O ataque teve como alvo pelo menos um membro sênior do Hezbollah, de acordo com fontes de segurança ouvidas pela agência Reuters.',
-      autoria: 'globo.com',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
